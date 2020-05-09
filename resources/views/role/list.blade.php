a928c3b2f8a3c3c234809b1e287f1a6d5a3c9c2b
Rashidli <rashidliseymur@gmail.com>
2020-05-09 22:05:15 +0400

    first

---
+++ resources/views/role/list.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ol>
+        @foreach($roles as $role)
+            <li>
+                <a href="{{ route('role.edit',$role->id) }}">{{ $role->name }}</a>
+                <a href="{{ route('role.permissions',$role->id) }}">Permissions</a>
+                <a style="color:red" href="{{ route('role.delete',$role->id) }}">Delete</a>
+            </li>
+        @endforeach
+    </ol>
+    <a href="{{ route('role.new') }}">add role</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin',function(){
+    return view('admin_login');
+});
+
+Route::post('/admin/login','LoginController@login')->name('admin.login');
+
+Route::group(['prefix' => 'role'], function () {
+    Route::get('/', 'RoleController@index')->name('role.list');
+    Route::get('/new', 'RoleController@new')->name('role.new');
+    Route::post('/add', 'RoleController@add')->name('role.add');
+    Route::get('/edit/{id}', 'RoleController@edit')->name('role.edit');
+    Route::post('/update/{id}', 'RoleController@update')->name('role.update');
+    Route::get('/delete/{id}', 'RoleController@delete')->name('role.delete');
+    Route::get('/permissions/{id}', 'RoleController@permissions')->name('role.permissions');
+    Route::post('/permissions/add/{id}', 'RoleController@permissionAdd')->name('role.permission.add');
+    Route::get('/permissions/delete/{id}/{permission_id}', 'RoleController@permissionDelete')->name('role.permission.delete');
+});
+
+Route::group(['prefix' => 'permission'], function () {
+    Route::get('/', 'PermissionController@index')->name('permission.list');
+    Route::get('/new', 'PermissionController@new')->name('permission.new');
+    Route::post('/add', 'PermissionController@add')->name('permission.add');
+    Route::get('/edit/{id}', 'PermissionController@edit')->name('permission.edit');
+    Route::post('/update/{id}', 'PermissionController@update')->name('permission.update');
+    Route::get('/delete/{id}', 'PermissionController@delete')->name('permission.delete');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
